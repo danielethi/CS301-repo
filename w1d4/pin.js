12bf72e08c53e32b533e60e2c027e6364cb0e7a4
danielethi <danielethi@outlook.com>
2021-02-25 20:27:08 -0600

    pin match check

---
+++ w1d4/pin.js
@@ -0,0 +1,27 @@
+
+//Question 3 
+const prompt = require('prompt-sync')();
+const pin = +prompt("insert pin: ");
+let accountPin=578;
+
+if(accountPin===pin){
+    console.log("Correct,welcome back.")
+   
+}else if(accountPin!==pin){
+
+    console.log("Incorrect,try again.");
+    +prompt("insert pin: ");
+    if(accountPin===pin){
+        console.log("Correct,welcome back.")
+    
+    }
+    if(accountPin!==pin){
+    console.log("Incorrect,try again.");
+    +prompt("insert pin: ");
+    }
+    console.log("Sorry you have been locked out");
+}else{
+
+}
+
+
