20318d0dd603cdcee703bb9d954e4ec3ac947ef7
danielethi <danielethi@outlook.com>
2021-02-26 19:56:00 -0600

    alternatives to default parameter

---
+++ w1d5/func.js
@@ -0,0 +1,8 @@
+Rules of naming a function are 
+1.function followed by name and parentheses();
+2.function name can contain letters ,gitits,underscores and dollar (same rules as variables);
+3.parameter names should be separated by comma.
+4.the code to be executed should be placed inside the curly brackets{ };
+5.parameters are placed inside a parenthesis.
+6.function arguments are values received by the function when it is invoked.
+7.Inside a function arguments behave like a local variables.
