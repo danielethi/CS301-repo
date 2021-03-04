e5dd59bda0a8d62b7219444052aa41abe5389901
danielethi <danielethi@outlook.com>
2021-03-03 20:20:05 -0600

    test ESLINT

--- w2d2/test.js
+++ w2d2/test.js
@@ -1,19 +1,11 @@
-//Question 1 find the greatest common factors of two numbers using js 
-let gcd = function(a, b) {
-    if (!b) {
-      return a;
+function isEven(n){
+  return n%2===0;
+}
+function printEven(n){
+  for(let i=1;i<=n;i++){
+    if(isEven(i)){
+      console.log(i)
     }
-  
-    return gcd(b, a % b);
   }
-  let result = gcd(20,28);
-  console.log(result);
-
-  /* this can be rewritten as 
-    funciton gcd(a,b){
-        if(!b){
-            return a;
-        }else{
-            return gcd(a%b);
-        }
-    */
+}
+printEven(10);
