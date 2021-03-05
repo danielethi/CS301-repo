995c94bb0fd23b146b0116d21ab3b687bd5d807e
danielethi <danielethi@outlook.com>
2021-03-04 19:36:31 -0600

    add  first and last
    this function will add the first and the last element of a given array.

---
+++ w2d4/arr.js
@@ -0,0 +1,9 @@
+//a function that returns the sum of the first and the last element in the array
+function addend(arr) {
+      let x=arr[0];
+      let y=arr[arr.length-1];
+      console.log(x+y);
+
+
+}
+addend([10,2,3,5]);
