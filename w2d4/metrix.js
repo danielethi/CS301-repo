536150cf0edd0b52705cde57a9fb962d4d51322b
danielethi <danielethi@outlook.com>
2021-03-05 12:04:10 -0600

    sum of metrix

--- w2d4/metrix.js
+++ w2d4/metrix.js
@@ -1,8 +1,9 @@
 //sum of metrix
-
+let sum=0
 let arr=[[1,2,3],[4,5,6],[7,8,9]];
 for(let i=0;i<arr.length;i++){
     for(let j=0;j<arr[i].length;j++){
-        console.log(arr[i][j]);
+        sum=sum+arr[i][j];
+        console.log(sum);
     }
 }
