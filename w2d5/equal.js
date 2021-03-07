3d8a0efad329afef8c4e622b9781708489d0b51c
danielethi <danielethi@outlook.com>
2021-03-06 23:10:13 -0600

    check array equality

---
+++ w2d5/equal.js
@@ -0,0 +1,24 @@
+
+//this checks if two arrays are equal or not
+
+function isArrayEqual(arr1,arr2){
+    if(arr1.length!=arr2.length){
+           return false;
+    }else{
+
+
+       for(let i=0;i<arr1.length;){
+           if(arr1[i]===arr2[i]){
+               return true;
+           }else{
+
+             return false;      
+           
+       }
+
+}
+
+    }
+}
+let result = isArrayEqual([1,2,3],[1,2,3]);
+console.log(result);
