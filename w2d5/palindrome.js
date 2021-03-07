f7c6129817f6815d449f139a01d3254a88611a6f
danielethi <danielethi@outlook.com>
2021-03-06 23:35:55 -0600

    nested array functions

---
+++ w2d5/palindrome.js
@@ -0,0 +1,30 @@
+
+let myArray=[];
+function checkPalindrome(arr){
+           for(let i=0;i<arr.length;i++){
+               myArray.push(arr.pop);
+           
+        
+              return  isArrayEqual(arr,myArray);
+           }
+
+
+function isArrayEqual(arr1,arr2){
+    if(arr1.length!=arr2.length){
+           return false;
+    }else{
+
+
+       for(let i=0;i<arr1.length;){
+           if(arr1[i]===arr2[i]){
+               return true;
+           }else{
+
+             return false;        
+       }
+}
+    }
+}
+}
+let result=checkPalindrome(["hi","Angi","hi"]);
+console.log(result);
