79cfe79422da9e051854fb32daef5b23b5ed8526
danielethi <danielethi@outlook.com>
2021-03-06 23:54:55 -0600

    transform array

--- w2d5/palindrome.js
+++ w2d5/palindrome.js
@@ -3,18 +3,12 @@ let myArray=[];
 function checkPalindrome(arr){
            for(let i=0;i<arr.length;i++){
                myArray.push(arr.pop);
-           
-        
               return  isArrayEqual(arr,myArray);
            }
-
-
 function isArrayEqual(arr1,arr2){
     if(arr1.length!=arr2.length){
            return false;
     }else{
-
-
        for(let i=0;i<arr1.length;){
            if(arr1[i]===arr2[i]){
                return true;
