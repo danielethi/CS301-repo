e142b5a586f6a11ec2a393c347dce49a7db2e76d
danielethi <danielethi@outlook.com>
2021-03-10 21:17:32 -0600

    simple calc in js

---
+++ w3d3/style.js
@@ -0,0 +1,26 @@
+function add(){
+ 
+    let x = document.getElementById("inp1").value;
+    let y = document.getElementById("inp2").value;
+ 
+
+    document.getElementById("demo").innerHTML=parseInt(x)+parseInt(y);
+
+}
+
+function sub(){
+
+let x = document.getElementById("inp1").Value;
+let y = document.getElementById("inp2").Value;
+
+document.getElementById("demo").innerHTML=x-y;
+
+}
+function mult(){
+
+let x = document.getElementById("inp1").Value;
+let y = document.getElementById("inp2").Value;
+
+document.getElementById("demo").innerHTML=x*y;
+
+}
