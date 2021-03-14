422278c084a893e6206bb9374dc10bfc4ee54f21
danielethi <danielethi@outlook.com>
2021-03-14 17:47:16 -0500

    reverse a string

--- w3d6/od.js
+++ w3d6/od.js
@@ -1,17 +1,19 @@
 
 //check if a number in array is perfectly odd(element is odd at odd indices) 
-function perfectlyOdd(arr){
-    for(let i=1;i<arr.length;i+=2){
-          if(isOdd(arr[i])===true){
-            return true;
-          }else{
-        return false;
-    }
 
-    }
-}
-function isOdd(n){
- return n%2!==0;
-}
-let result = perfectlyOdd([2,5,11,61,32,21,5,3,7]);
-console.log(result);
+
+// function perfectlyOdd(arr){
+//     for(let i=1;i<arr.length;i+=2){
+//           if(isOdd(arr[i])===true){
+//             return true;
+//           }else{
+//         return false;
+//     }
+
+//     }
+// }
+// function isOdd(n){
+//  return n%2!==0;
+// }
+// let result = perfectlyOdd([2,5,11,61,32,21,5,3,7]);
+// console.log(result);
---
+++ w3d6/rvc.js
@@ -0,0 +1,7 @@
+//this will reverse a given string
+let str ="daniel";
+let copy = " "
+for(let i=0;i<str.length;i++){
+  copy+=str[str.length - i - 1];
+}
+console.log(copy) //leinad
